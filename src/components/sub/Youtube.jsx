3da9c855d9ad5768c97923786b9b0ce0968f7bbc
reeyounghyun <115164041+reeyounghyun@users.noreply.github.com>
2023-03-26 11:55:33 +0900

    youtube data fetching

--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -1,9 +1,32 @@
-import Layout from ' ../common/Layout';
+import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Youtube() {
+	const [Vids, setVids] = useState([]);
+
+	useEffect(() => {
+		const key = 'AIzaSyAGpWq7AF0NYZp55xSg6n3WzYimiqnbLzo';
+		const playlistId = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&maxResults=10&playlistId=${playlistId}`;
+
+		axios.get(url).then((data) => {
+			console.log(data.data.items);
+			setVids(data.data.items);
+		});
+	}, []);
+
 	return (
-		<Layout name = {'Youtube'}>
-			<p>커뮤니티 페이지 내용이 들어올 자리</p>
+		<Layout name={'Youtube'}>
+			{Vids.map((vid, idx) => {
+				return (
+					<article key={idx}>
+						<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+						<h2>{vid.snippet.title}</h2>
+						<p>{vid.snippet.description}</p>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
