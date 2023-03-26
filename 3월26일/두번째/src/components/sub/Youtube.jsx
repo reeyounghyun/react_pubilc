6295657a01d50487b146d11c46bd7c35985fb4cb
reeyounghyun <115164041+reeyounghyun@users.noreply.github.com>
2023-03-26 11:16:13 +0900

    헤더 메인, 서브 스타일 분리

---
+++ 3월26일/두번째/src/components/sub/Youtube.jsx
@@ -0,0 +1,11 @@
+import Layout from ' ../common/Layout';
+
+function Youtube() {
+	return (
+		<Layout name = {'Youtube'}>
+			<p>커뮤니티 페이지 내용이 들어올 자리</p>
+		</Layout>
+	);
+}
+
+export default Youtube;
