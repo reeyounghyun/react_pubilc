90b8af132062bd7c514e4c190e6fbe6b3152525b
reeyounghyun <115164041+reeyounghyun@users.noreply.github.com>
2023-03-26 11:27:21 +0900

    썸네일 호버효과 적용

---
+++ src/components/sub/Gallery.jsx
@@ -0,0 +1,11 @@
+import Layout from ' ../common/Layout';
+
+function Gallery() {
+	return (
+		<Layout name = {'Gallery'}>
+			<p>커뮤니티 페이지 내용이 들어올 자리</p>
+		</Layout>
+	);
+}
+
+export default Gallery;
---
+++ src/components/sub/Location.jsx
@@ -0,0 +1,11 @@
+import Layout from ' ../common/Layout';
+
+function Location() {
+	return (
+		<Layout name = {'Location'}>
+			<p>커뮤니티 페이지 내용이 들어올 자리</p>
+		</Layout>
+	);
+}
+
+export default Location;
